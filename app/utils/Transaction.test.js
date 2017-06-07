d844a4b6f828bc25563bcab6af44c4a02dfb38bd
mcifani <mcifani@users.noreply.github.com>
2017-06-07 17:26:54 -0400

    New transaction mocha test spec file

---
+++ app/utils/Transaction.test.js
@@ -0,0 +1,14 @@
+import assert from 'assert'
+
+describe('Transaction Wallet', () => {
+    // it('all',() => {
+    //
+    // })
+    it('should detect private key in memo', () => {
+
+    })
+    it('should detect password in memo', () => {
+
+    })
+})
+
