4fda90c66bb47a00ec396099915e0d53fa112aba
ludorum <soosmail@me.com>
2018-04-27 03:19:29 +0900

    DMCA Bad User add

--- src/app/utils/DMCAUserList.js
+++ src/app/utils/DMCAUserList.js
@@ -11,6 +11,15 @@ mariarose4568
 hd-movie-watch
 keilamve
 rayjason
+mala99kz
+alvaroamel
+lobadosasia
+official-tvmuse
+rathor1
+chirag1
+anuchaudhary546
+azki
+dvdripper
 `.trim().split('\n');
 
 export default list;
