be78b8410dd9cb1b127e7423541a6331c6335e13
ludorum <soosmail@me.com>
2018-04-26 22:15:32 +0900

    docker compose update.

--- up.sh
+++ up.sh
@@ -4,14 +4,14 @@ EXIST_BLUE=$(docker-compose -f docker-compose.mysql.yml -f docker-compose.blue.y
 
 if [ -z "$EXIST_BLUE" ]; then
     docker-compose -f docker-compose.mysql.yml -f docker-compose.blue.yml pull
-    docker-compose -f docker-compose.mysql.yml -f docker-compose.blue.yml up -d
+    docker-compose -f docker-compose.mysql.yml -f docker-compose.blue.yml up -d --build
 
     sleep 10
 
     docker-compose  -f docker-compose.mysql.yml -f docker-compose.green.yml stop server_green
 else
     docker-compose -f docker-compose.mysql.yml -f docker-compose.green.yml pull
-    docker-compose -f docker-compose.mysql.yml -f docker-compose.green.yml up -d
+    docker-compose -f docker-compose.mysql.yml -f docker-compose.green.yml up -d --build
 
     sleep 10
 
